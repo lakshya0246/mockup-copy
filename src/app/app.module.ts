6fe680ab0e0d3f8f8e1408caff3a0be230a64c85
Lakshya <lakshya.dahiya@onpress9.com>
2021-12-13 17:40:59 +0530

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { OngoingApplicationsListComponent } from './components/ongoing-applications-list/ongoing-applications-list.component';
+import { FilterTabsComponent } from './components/filter-tabs/filter-tabs.component';
+import { OpportunitiesListComponent } from './components/opportunities-list/opportunities-list.component';
+import { IconComponent } from './components/icon/icon.component';
+import { NotificationsListComponent } from './components/notifications-list/notifications-list.component';
 
 @NgModule({
-  declarations: [AppComponent, UserDetailsComponent, SidebarComponent],
+  declarations: [AppComponent, UserDetailsComponent, SidebarComponent, OngoingApplicationsListComponent, FilterTabsComponent, OpportunitiesListComponent, IconComponent, NotificationsListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
