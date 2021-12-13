a7d1e3bab25de723bf8fcbcfa62fe0e04cad2fc3
Lakshya <lakshya.dahiya@onpress9.com>
2021-12-13 17:39:14 +0530

    Update mocks

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
-interface Notification {
+export interface Notification {
   id: string;
   title: string;
   description: string;
@@ -20,54 +20,133 @@ interface User {
   profilePictureUrl: string;
 }
 
-interface Application {
+export interface Application {
   id: string;
   title: string;
+  company: string;
   startDate: Date;
   questionType: string;
   durationDisplay: string;
   durationInMinutes: number;
   venue: string;
+  type: string;
 }
 
-interface Opportunity {
+export interface Opportunity {
   id: string;
   title: string;
+  company: string;
   salary: string;
   startDate: Date;
-  isTrending: boolean;
+  isTrending?: boolean;
   isUserInterested?: boolean;
   location: string;
   jobType: 'full time' | 'part time';
 }
 
+export const OPPORTUNITIES: Opportunity[] = [
+  {
+    id: uuid(),
+    company: 'SBI',
+    title: 'Marketing Analyst',
+    salary: 'INR 30, 000/m',
+    startDate: new Date(),
+    jobType: 'full time',
+    location: 'Bangalore',
+  },
+  {
+    id: uuid(),
+    company: 'SBI',
+    title: 'Marketing Analyst',
+    salary: 'INR 30, 000/m',
+    startDate: new Date(),
+    jobType: 'full time',
+    location: 'Bangalore',
+    isTrending: true,
+  },
+  {
+    id: uuid(),
+    company: 'SBI',
+    title: 'Marketing Analyst',
+    salary: 'INR 30, 000/m',
+    startDate: new Date(),
+    jobType: 'full time',
+    location: 'Bangalore',
+    isUserInterested: false,
+  },
+  {
+    id: uuid(),
+    company: 'SBI',
+    title: 'Marketing Analyst',
+    salary: 'INR 30, 000/m',
+    startDate: new Date(),
+    jobType: 'full time',
+    location: 'Bangalore',
+  },
+];
+
+export const ONGOING_APPLICATIONS: Application[] = [
+  {
+    id: uuid(),
+    title: 'Marketing Analyst',
+    company: 'SBI',
+    startDate: new Date(),
+    questionType: 'MCQ',
+    durationDisplay: '2 Hrs',
+    durationInMinutes: 120,
+    venue: 'Andheri Nagar, Sec 4, Mumbai',
+    type: 'Test',
+  },
+  {
+    id: uuid(),
+    title: 'Marketing Analyst',
+    company: 'SBI',
+    startDate: new Date(),
+    questionType: 'MCQ',
+    durationDisplay: '2 Hrs',
+    durationInMinutes: 120,
+    type: 'Test',
+    venue: 'Andheri Nagar, Sec 4, Mumbai',
+  },
+  {
+    id: uuid(),
+    title: 'Marketing Analyst',
+    company: 'SBI',
+    startDate: new Date(),
+    questionType: 'MCQ',
+    durationDisplay: '2 Hrs',
+    durationInMinutes: 120,
+    venue: 'Andheri Nagar, Sec 4, Mumbai',
+    type: 'Test',
+  },
+];
 export const NOTIFICATIONS: Notification[] = [
   {
     id: uuid(),
     title: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`,
-    createdAt: new Date(),
+    description: `Lorem ipsum dolor sit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`,
+    createdAt: new Date(Date.now() - 2 * 60 * 60 * 1000),
     type: 'apple',
   },
   {
     id: uuid(),
-    title: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
+    title: `consectetur adipiscing elit`,
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`,
-    createdAt: new Date(),
+    createdAt: new Date(Date.now() - 5 * 60 * 60 * 1000),
     type: 'banana',
   },
   {
     id: uuid(),
-    title: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
+    title: `Lorem ipsum dolor`,
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`,
-    createdAt: new Date(),
+    createdAt: new Date(Date.now() - 11 * 60 * 60 * 1000),
     type: 'caramel',
   },
   {
     id: uuid(),
-    title: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`,
-    createdAt: new Date(),
+    title: `I dolor sit amet, consectetur adipiscing elit`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
+    createdAt: new Date(Date.now() - 11 * 60 * 60 * 1000),
     type: 'dolphin',
   },
 ];
